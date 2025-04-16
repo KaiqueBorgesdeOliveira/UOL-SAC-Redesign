7783e40ed689384dc768eb1397f248f9c01673e6
KaiqueBorgesdeOliveira <kaique.borges.1995@gmail.com>
2025-04-16 12:16:32 -0300

    chore: adicionar arquivos iniciais do projeto

---
+++ src/utils/App.tsx
@@ -0,0 +1,11 @@
+// Novo conteúdo inicial:
+import { GlobalStyle } from '../styles/global';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      {/* Seu conteúdo aqui */}
+    </>
+  );
+}
